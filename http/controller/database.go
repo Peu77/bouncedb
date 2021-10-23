5b0d6c42f79fb323370eeb5325e0a73c0d8cd9d1
peu77 <emilebert@icloud.com>
2021-10-23 21:18:31 +0000

    add path variables to database request

--- http/controller/database.go
+++ http/controller/database.go
@@ -7,24 +7,28 @@ import (
 
 func Insert(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "POST" {
-		fmt.Fprintf(writer, "insert "+request.Method+"\n")
+		database := request.URL.Path[len("/insert/"):]
+		fmt.Fprintf(writer, "insert database: "+database+"\n")
 	}
 }
 
 func Update(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "PUT" {
-		fmt.Fprintf(writer, "update "+request.Method+"\n")
+		database := request.URL.Path[len("/update/"):]
+		fmt.Fprintf(writer, "update "+request.Method+" "+database+"\n")
 	}
 }
 
 func Delete(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "DELETE" {
-		fmt.Fprintf(writer, "delete "+request.Method+"\n")
+		database := request.URL.Path[len("/delete/"):]
+		fmt.Fprintf(writer, "delete "+request.Method+" "+database+" \n")
 	}
 }
 
 func Get(writer http.ResponseWriter, request *http.Request) {
 	if request.Method == "GET" {
-		fmt.Fprintf(writer, "get "+request.Method+"\n")
+		database := request.URL.Path[len("/get/"):]
+		fmt.Fprintf(writer, "get "+request.Method+" "+database+"\n")
 	}
 }
--- http/http.go
+++ http/http.go
@@ -7,7 +7,7 @@ import (
 )
 
 func Http() {
-	http.HandleFunc("/insert", controller.Insert)
+	http.HandleFunc("/insert/", controller.Insert)
 	http.HandleFunc("/update", controller.Update)
 	http.HandleFunc("/delete", controller.Delete)
 	http.HandleFunc("/get", controller.Get)
