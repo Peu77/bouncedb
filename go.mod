7d7ee43e85cdfde4626f261848becbfd709ee390
niclas <bergerbtw@gmail.com>
2021-10-24 14:22:22 +0200

    Removed useless format, due to too much similarity to real json

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module bouncedb
 
 go 1.17
+
+require github.com/goccy/go-json v0.7.10 // indirect
--- main.go
+++ main.go
@@ -2,24 +2,11 @@ package main
 
 import (
 	"bouncedb/file"
-	"bouncedb/format"
 	"bouncedb/http"
 	"bouncedb/user"
-	"fmt"
 )
 
-type Test struct {
-	name    string
-	test    int
-	boolean bool
-}
-
 func main() {
-	// json := format.ToJsonObject("test: 1337, naruto: \"Super Intense Minecraft Player 😎 I'm a SIMP.\" ")
-	v := Test{test: 1, name: "Peter", boolean: false}
-
-	fmt.Println(format.FromStructToJson(v))
-
 	file.InitFiles()
 	go http.Http()
 	user.User()
